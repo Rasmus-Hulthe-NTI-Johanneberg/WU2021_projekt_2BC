aa4465c93fc574cc05d920e74b0e01512ca3fb7c
rasmus-hulthe-nti-johanneberg <rasmus.hulthe@elev.ga.ntig.se>
2021-04-03 00:23:31 +0200

    side bar button working as intended aswell as better mobile support and general cleaning of code

--- js/main.js
+++ js/main.js
@@ -5,18 +5,13 @@ function show(){
 console.log(showing_val)
 showing_val = !showing_val
 if (showing_val){
-    document.querySelector(".grid-wrapper").style.gridTemplateAreas = '"bt sb" "b1 b1" "b2 b2" "f1 f2" "f3 f4" "fw fw"'
-    document.querySelector(".grid-wrapper").style.gridTemplateColumns = ""
+    document.querySelector("#grid-wrapper").style.gridTemplateAreas = '"bt sb" "b1 b1" "b2 b2" "f1 f2" "f3 f4" "fw fw"'
+    document.querySelector("#grid-wrapper").style.gridTemplateColumns = ""
     document.querySelector(".navbar").style.display = 'none'
-    console.log('rätt')
 } else{
-    document.querySelector(".grid-wrapper").style.gridTemplateAreas = '"nv sb sb" "nv b1 b1" "nv b2 b2" "nv f1 f2" "nv f3 f4" "fw fw fw"'
-    console.log(document.querySelector(".grid-wrapper").style.gridTemplateColumns)
-    document.querySelector(".grid-wrapper").style.gridTemplateColumns = "50%"
-    console.log(document.querySelector(".grid-wrapper").style.gridTemplateColumns)
-    document.querySelector(".navbar").style.display = 'grid'
-    console.log('fel')
-}
+    document.querySelector("#grid-wrapper").style.gridTemplateAreas = '"nv sb sb" "nv b1 b1" "nv b2 b2" "nv f1 f2" "nv f3 f4" "fw fw fw"'
+    document.querySelector("#grid-wrapper").style.gridTemplateColumns = "50%"
+    document.querySelector(".navbar").style.display = 'grid'}
 }
 document.addEventListener('DOMContentLoaded', () => 
 {
